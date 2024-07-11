ae60f64ce2bcfbcb1247d1902905d498bd138d2d
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-07-12 00:32:04 +0400

    merge

---
+++ ChitAuto/ChitAuto/Scenes/AddCarDetails/Sheets/ReleaseDateSheet/ReleaseDateView.swift
@@ -0,0 +1,68 @@
+//
+//  ReleaseDateView.swift
+//  ChitAuto
+//
+//  Created by Temur Chitashvili on 11.07.24.
+//
+
+import UIKit
+
+final class ReleaseDateView: UIView {
+    //MARK: - UIComponents
+    private let titleLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "გამოშვები წელი"
+        lbl.textColor = .label
+        lbl.font = .boldSystemFont(ofSize: 20)
+        lbl.textAlignment = .center
+        return lbl
+    }()
+    
+    let releaseDateTableView: UITableView = {
+        let tblView = UITableView()
+        tblView.translatesAutoresizingMaskIntoConstraints = false
+        tblView.register(ReleaseDateCell.self, forCellReuseIdentifier: ReleaseDateCell.identifier)
+        tblView.separatorStyle = .none
+        return tblView
+    }()
+    
+    //MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .customBackground
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Setup UI
+    private func setupUI() {
+        setupTitleLabel()
+        setModelsTableView()
+    }
+    
+    //MARK: - Set UI Components
+    private func setupTitleLabel() {
+         addSubview(titleLabel)
+
+        NSLayoutConstraint.activate([
+             titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+             titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+         ])
+     }
+    
+    private func setModelsTableView() {
+        addSubview(releaseDateTableView)
+        
+        //Set Constraints
+        NSLayoutConstraint.activate([
+            releaseDateTableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            releaseDateTableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            releaseDateTableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
+            releaseDateTableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+}
